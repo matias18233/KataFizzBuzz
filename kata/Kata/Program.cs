0a7974e99de322928dcfde28263e44ca2ef1066e
Fernando Matías Cruz <matias18233@gmail.com>
2020-12-14 00:38:45 -0300

    Se agrega la implementación y pruebas

---
+++ kata/Kata/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Kata {
+    public class Program {
+        public static void Main(string[] args) {
+            foreach (String cadena in obtenerListado()) {
+                Console.WriteLine(cadena);
+            }
+            Console.ReadKey();
+        }
+        public static string[] obtenerListado() {
+            string[] listado = new string[20];
+            for (int i = 1; i <= 20; i++) {
+                if ((obtenerFizz(i)) && (obtenerBuzz(i))) {
+                    listado[i - 1] = "FizzBuzz";
+                } else if (obtenerFizz(i)) {
+                    listado[i - 1] = "Fizz";
+                } else if (obtenerBuzz(i)) {
+                    listado[i - 1] = "Buzz";
+                } else {
+                    listado[i - 1] = Convert.ToString(i);
+                }
+            }
+            return listado;
+        }
+        private static bool obtenerFizz(int numero) {
+            if (numero % 3 == 0) {
+                return true;
+            }
+            return false;
+        }
+        private static bool obtenerBuzz(int numero) {
+            if (numero % 5 == 0) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ kata/KataTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void controlarFizz() {
+            String[] listado = Kata.Program.obtenerListado();
+            Assert.AreEqual(listado[5], "Fizz");
+        }
+        [TestMethod]
+        public void controlarBuzz() {
+            String[] listado = Kata.Program.obtenerListado();
+            Assert.AreEqual(listado[9], "Buzz");
+        }
+        [TestMethod]
+        public void controlarFizzBuzz() {
+            String[] listado = Kata.Program.obtenerListado();
+            Assert.AreEqual(listado[14], "FizzBuzz");
+        }
+        [TestMethod]
+        public void controlarPosicion16() {
+            String[] listado = Kata.Program.obtenerListado();
+            Assert.AreEqual(listado[16], "17");
+        }
+        [TestMethod]
+        public void controlarPosicion18() {
+            String[] listado = Kata.Program.obtenerListado();
+            Assert.AreEqual(listado[18], "19");
+        }
+    }
+}
